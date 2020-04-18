caf54f7f0b26bda9c2920277dcd963645e8d1e9d
Daniel Munday <dmunday01@gmail.com>
2020-04-18 18:41:19 +0200

    Dan (#8)
    * tf
    
    * Added test stage with updated collision boxes
    
    * Added camera
    
    * ???
    
    Co-authored-by: Daniel Munday <daniel.munday@sprinthive.com>

---
+++ src/scripts/camera/CameraPivot.gd
@@ -0,0 +1,12 @@
+extends Position2D
+
+onready var parent = $'..'
+
+func _ready():
+	update_pivot_angle()
+	
+func _physics_process(delta):
+	update_pivot_angle()
+	
+func update_pivot_angle():
+	rotation = parent.look_direction.angle()
