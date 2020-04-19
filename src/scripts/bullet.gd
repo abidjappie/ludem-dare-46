613f58a7dc46a85c69e55244ff6a856927b00557
Daniel Munday <daniel.munday@sprinthive.com>
2020-04-19 17:59:07 +0200

    Added player and enemy bullets

--- src/scripts/bullet.gd
+++ src/scripts/bullet.gd
@@ -2,12 +2,13 @@ extends Area2D
 
 # apply a velocity
 export var velocity = Vector2()
+
 # Called when the node enters the scene tree for the first time.
+
 func _ready():
 	set_process(true)
-	yield(get_node("VisibilityNotifier2D"), "exit_screen")
+	yield(get_node("VisibilityNotifier2D"), "screen_exited")
 	queue_free() # destroy bullet
-	pass # Replace with function body.
 
 func _process(delta):
 	translate(velocity * delta)
