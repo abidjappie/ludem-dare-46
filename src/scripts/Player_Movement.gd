87a14ec46f1fcfaf1112116d624c01d8b97a7bc1
abidjappie <56756836+abidjappie@users.noreply.github.com>
2020-04-19 02:39:20 +0200

    Mad tings (#17)
    * Did big tings.
    
    * Bugfix
    
    * Various changes to stage and BG.

--- src/scripts/Player_Movement.gd
+++ src/scripts/Player_Movement.gd
@@ -1,8 +1,6 @@
 extends KinematicBody2D
 
-const WALK_SPEED = 1000
-const WALK_MAX_SPEED = 100
-const STOP_FORCE = 800
+const WALK_SPEED = 100
 const JUMP_SPEED = 270
 const GRAVITY = 650
 
@@ -11,64 +9,59 @@ var look_direction = Vector2(1,0)
 
 var velocity = Vector2()
 
-onready var body = get_node("PlayerBodySprite")
-onready var armFront = get_node("PlayerArmFrontSprite")
-onready var armBack = get_node("PlayerArmBackSprite")
-
 func ready():
-	body.play("idle")
-	body.speed_scale = 1
-	armFront.visibility = true
+	$body.play("idle")
+	$body.speed_scale = 1
+	$armFront.visibility = true
 	
 func shoot():
 	if Input.is_action_just_pressed("shoot"):
-		if armFront.visible:
-			armFront.play("Fire")
-			yield(armFront, "animation_finished")
-			armFront.play("Return")
-			yield(armFront, "animation_finished")
-			armFront.play("idle")
+		if $armFront.visible:
+			$armFront.play("Fire")
+			yield($armFront, "animation_finished")
+			$armFront.play("Return")
+			yield($armFront, "animation_finished")
+			$armFront.play("idle")
 		else:
-			armBack.play("Fire")
-			yield(armBack, "animation_finished")
-			armBack.play("Return")
-			yield(armBack, "animation_finished")
-			armBack.play("idle")
+			$armBack.play("Fire")
+			yield($armBack, "animation_finished")
+			$armBack.play("Return")
+			yield($armBack, "animation_finished")
+			$armBack.play("idle")
 
 # play animations
 func _process(_delta):
 	shoot()
 	if Input.is_action_pressed("right"):
-		$PlayerBodySprite.speed_scale = 2
-		$PlayerBodySprite.flip_h = false
-		$PlayerArmFrontSprite.show()
-		$PlayerArmBackSprite.hide()
-		$PlayerBodySprite.play("Walk")
+		$body.speed_scale = 2
+		$body.flip_h = false
+		$body.set_offset(Vector2(6, 0))
+		$armFront.show()
+		$armBack.hide()
+		$body.play("Walk")
 	elif Input.is_action_pressed("left"):
-		$PlayerBodySprite.speed_scale = 2
-		$PlayerArmFrontSprite.hide()
-		$PlayerArmBackSprite.show()
-		$PlayerBodySprite.flip_h = true
-		$PlayerArmBackSprite.flip_h = true
-		$PlayerBodySprite.play("Walk")
+		$body.speed_scale = 2
+		$body.flip_h = true
+		$body.set_offset(Vector2(-6, 0))
+		$armBack.set_offset(Vector2(-10, 0))
+		$armFront.hide()
+		$armBack.show()
+		$armBack.flip_h = true
+		$body.play("Walk")
 	elif Input.is_action_just_pressed("jump"):
-		$PlayerBodySprite.speed_scale = 1
-		$PlayerBodySprite.play("Jump")
+		$body.speed_scale = 1
+		$body.play("Jump")
 	elif is_on_floor():
-		$PlayerBodySprite.speed_scale = 1
-		$PlayerBodySprite.play("idle")
+		$body.speed_scale = 1
+		$body.play("idle")
 
 func _physics_process(delta):
-	var walk = WALK_SPEED * (Input.get_action_strength("right") - Input.get_action_strength("left"))
-	
-	# slow the player down if they're not attempting to move
-	if abs(walk) < WALK_SPEED:
-		velocity.x = move_toward(velocity.x, 0, STOP_FORCE * delta)
+	if Input.is_action_pressed("right"):
+		velocity.x = WALK_SPEED
+	elif Input.is_action_pressed("left"):
+		velocity.x = -WALK_SPEED
 	else:
-		velocity.x += walk * delta
-	
-	# clamp the maximum horizontal movement speed
-	velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
+		velocity.x = 0
 	
 	# apply gravity
 	velocity.y += delta * GRAVITY
