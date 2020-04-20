c85a17bd1f2bc250dfe48f397b709d728298434a
Abid Jappie <abidjappie@live.com>
2020-04-21 01:32:38 +0200

    Added stuff to screens

--- src/scripts/Screens.gd
+++ src/scripts/Screens.gd
@@ -2,14 +2,20 @@ extends ParallaxBackground
 
 signal reboot_signal
 
+var playing = false
+
 func _ready():
 	$AnimationPlayer.play("FadeOut")
 	$GameoverScreen.visible = false
+	$GameendScreen.visible = false
 
 func _process(_delta):
 	if $GameoverScreen.visible and Input.is_action_just_pressed("shoot"):
 		$AnimationPlayer.play("FadeInRest")
 		yield($AnimationPlayer, "animation_finished")
+		emit_signal("reboot_signal")
+		
+	if $GameendScreen.visible and Input.is_action_just_pressed("shoot") and !playing:
 		emit_signal("reboot_signal")
 
 func show_gameover():
@@ -20,3 +26,14 @@ func show_gameover():
 	yield($GameoverScreen, "animation_finished")
 	$GameoverScreen.stop()
 	$GameoverScreen.frame = 1
+
+func show_gameend(is_good):
+	playing = true
+	$GameendScreen.show()
+	$GameendScreen.set_speed_scale(0.75)
+	if is_good: 
+		$GameendScreen.play("GoodEnd")
+	else: 
+		$GameendScreen.play("BadEnd")
+	yield($GameendScreen, "animation_finished")
+	playing = false
--- src/scripts/Titlescreen.gd
+++ src/scripts/Titlescreen.gd
@@ -17,20 +17,41 @@ func _ready():
 	fade_duration, transition_type, Tween.EASE_IN,0)
 	fade_in.start()
 	background.play("1Init")
+	background.set_speed_scale(1)
 	yield(background, "animation_finished")
 	background.play("2Loading")
+	background.set_speed_scale(0.5)
 	yield(background, "animation_finished")
 	background.play("3DiskOK")
+	background.set_speed_scale(0.8)
 	yield(background, "animation_finished")
 	short_noise.play()
 	background.play("46Flash")
+	background.set_speed_scale(1.5)
 	yield(background, "animation_finished")
 	background.play("5PressStart")
+	background.set_speed_scale(1)
 	yield(background, "animation_finished")
+	background.animation="5PressStart"
+	background.stop()
+	background.frame = 5
 	
 func _process(_delta):
 	if Input.is_action_just_pressed("shoot"):
-		background.play("46Flash")
-		yield(background, "animation_finished")
+		if (background.animation == "5PressStart" and background.frame == 5):
+			background.play("46Flash")
+			background.set_speed_scale(1.5)
+			yield(background, "animation_finished")
+			background.play("7KeysWait")
+			background.set_speed_scale(1)
+			yield(background, "animation_finished")
+			background.animation = "7KeysWait"
+			background.stop()
+			background.frame = 2
+		elif (background.animation == "7KeysWait" and background.frame == 2):
+			background.play("8FadeOut")
+			background.set_speed_scale(1)
+			yield(background, "animation_finished")
+		
 # warning-ignore:return_value_discarded
-		get_tree().change_scene("res://src/scenes/Default_Scene.tscn")
+			get_tree().change_scene("res://src/scenes/Default_Scene.tscn")
