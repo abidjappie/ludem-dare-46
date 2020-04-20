de97537f32b1abef64f44802e850267185129816
Abid Jappie <abidjappie@live.com>
2020-04-20 22:40:38 +0200

    Completed IFV.

--- src/scripts/IFVController.gd
+++ src/scripts/IFVController.gd
@@ -5,13 +5,25 @@ const WALK_MAX_SPEED = 150
 const STOP_FORCE = 800
 const JUMP_SPEED = 400
 const GRAVITY = 700
+const BULLET_SPEED = 400
+const VISION = 140
+const RANGE = 110
+const RANGE_T = 120
+const BLINDSPOT = 64
 
+var has_person = true
 var is_alive = true
 var HEALTH = 3
 
 var animation = ['Move', 'Fire']
 var velocity = Vector2()
 var player
+
+var shooting = false
+var shooting_t = false
+var reversing = false
+
+const scn_bullet = preload("res://src/scenes/bullet.tscn")
 
 func _ready():
 	player = get_node("../Player_KinematicBody2D")
@@ -22,29 +34,72 @@ func _physics_process(delta):
 		var height_from_player = Vector2(0, position.y).distance_to(Vector2(0, player.position.y))
 		
 		# If player is within range
-		if (distance_from_player < 128 && distance_from_player > 64):
+		if (distance_from_player < VISION && distance_from_player > RANGE):
 			if (player.position.x > position.x):
 				velocity.x = 10
-				if ($body.animation != "Move"):
-					$body.animation = "Move"
 				$body.flip_h = false
 				$turret.flip_h = false
 				$body.set_offset(Vector2(3, -4))
 				$turret.set_offset(Vector2(-3, -11))
 			else:
 				velocity.x = -10
-				if ($body.animation != "Move"):
-					$body.animation = "Move"
 				$body.flip_h = true
 				$turret.flip_h = true
 				$body.set_offset(Vector2(-3, -4))
 				$turret.set_offset(Vector2(3, -11))
+			reversing = false
+		elif (distance_from_player < RANGE && distance_from_player > BLINDSPOT && height_from_player < 16):
+			if (player.position.x > position.x):
+				$body.flip_h = false
+				$turret.flip_h = false
+				$body.set_offset(Vector2(3, -4))
+				$turret.set_offset(Vector2(-3, -11))
+			else:
+				$body.flip_h = true
+				$turret.flip_h = true
+				$body.set_offset(Vector2(-3, -4))
+				$turret.set_offset(Vector2(3, -11))
+			velocity.x = 0
+			if ($body.animation != "Fire"):
+					$body.animation = "Fire"
+					$body.play()
+					$body.set_speed_scale(0.75)
+		elif (distance_from_player < BLINDSPOT && height_from_player < 16):
+			if (player.position.x > position.x):
+				velocity.x = -10
+				$body.flip_h = false
+				$turret.flip_h = false
+				$body.set_offset(Vector2(3, -4))
+				$turret.set_offset(Vector2(-3, -11))
+			else:
+				velocity.x = 10
+				$body.flip_h = true
+				$turret.flip_h = true
+				$body.set_offset(Vector2(-3, -4))
+				$turret.set_offset(Vector2(3, -11))
+			if !reversing:
+				reversing = true
 		else:
 			velocity.x = 0
-			$body.stop()
-			
-		if (velocity.x!=0):
-			$body.play()
+			if is_alive:
+				$body.animation = "Move"
+				$body.stop()
+		
+		if (distance_from_player < RANGE_T && height_from_player < 18):
+			if ($turret.animation != "Fire"):
+				$turret.animation = "Fire"
+				$turret.play()
+				$turret.set_speed_scale(1)
+		else:
+			$turret.animation = "Idle"
+		
+		if velocity.x != 0:
+			if ($body.animation != "Move"):
+				$body.play("Move", reversing)
+				
+		shoot("turret")
+		shoot("body")
+		
 		# clamp the maximum horizontal movement speed
 		velocity.x = clamp(velocity.x, -WALK_MAX_SPEED, WALK_MAX_SPEED)
 		
@@ -54,8 +109,31 @@ func _physics_process(delta):
 		velocity = move_and_slide_with_snap(velocity, Vector2.DOWN, Vector2.UP)
 		
 		position += velocity * delta
-	if HEALTH <= 0:
-		die()
+
+func shoot(from):
+	var sprite = get_node(from)
+	if (sprite.animation == "Fire"):	
+		if sprite.get_frame() == 1 and ((from == "body" and !shooting) or (from == "turret" and !shooting_t)):
+			var bullet = scn_bullet.instance()
+			var bullet_height = 2
+			if from == "body": 
+				shooting = true
+			if from == "turret": 
+				shooting_t = true
+				bullet_height = 8
+			bullet.from_who = get_name()
+			bullet.get_node("AnimatedSprite").animation = "bullet"
+			bullet.position = get_node(".").position
+			bullet.position.y -= bullet_height
+			if sprite.flip_h:
+				bullet.velocity.x = -BULLET_SPEED
+			else:
+				bullet.velocity.x = BULLET_SPEED
+			$'..'.add_child(bullet)
+		elif (sprite.get_frame() == 0):
+			if from == "body": shooting = false
+			if from == "turret": shooting_t = false
+
 
 # kill this enemy
 func die():
